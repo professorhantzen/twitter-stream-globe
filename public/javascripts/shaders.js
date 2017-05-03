2d7331d6ceadcae21308780f079deb9624e4bafc
professorhantzen <professorhantzen@users.noreply.github.com>
2017-05-03 18:50:02 +0200

    Update shaders.js

--- public/javascripts/shaders.js
+++ public/javascripts/shaders.js
@@ -20,7 +20,7 @@ var Shaders = {
         'vec3 diffuse = texture2D( texture, vUv ).xyz;',
         'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
         'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
-        'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
+        'gl_FragColor = vec4( diffuse + atmosphere, 0.7 );',
       '}'
     ].join('\n')
   },
@@ -37,7 +37,7 @@ var Shaders = {
       'varying vec3 vNormal;',
       'void main() {',
         'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
-        'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
+        'gl_FragColor = vec4( 1.0, 1.0, 1.0, 0.7 ) * intensity;',
       '}'
     ].join('\n')
   }
